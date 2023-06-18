40b7569b0c16848ff598ed997bef04b9145bd658
AlexGrohmann <alex.grohmann@googlemail.com>
2023-06-18 20:18:57 +0200

    comparison

---
+++ operators/operators5.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var x = 5
+	var y = 3
+	fmt.Println(x > y) // returns 1 (true) because 5 is greater than 3
+}
+
+// ==	Equal to	x == y
+// !=	Not equal	x != y
+// >	Greater than	x > y
+// <	Less than	x < y
+// >=	Greater than or equal to	x >= y
+// <=	Less than or equal to	x <= y
