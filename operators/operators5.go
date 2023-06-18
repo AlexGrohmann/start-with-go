d464f52e75f8581f662ef94a2ba9b3f972d8f50b
AlexGrohmann <alex.grohmann@googlemail.com>
2023-06-18 20:20:32 +0200

    add comment for bitwise operators

--- operators/operators5.go
+++ operators/operators5.go
@@ -22,3 +22,11 @@ func main() {
 // && 	Logical and	Returns true if both statements are true	x < 5 &&  x < 10
 // || 	Logical or	Returns true if one of the statements is true	x < 5 || x < 4
 // !	Logical not	Reverse the result, returns false if the result is true	!(x < 5 && x < 10)
+
+//Bitwise operators
+
+// & 	AND	Sets each bit to 1 if both bits are 1	x & y
+// |	OR	Sets each bit to 1 if one of two bits is 1	x | y
+//  ^	XOR	Sets each bit to 1 if only one of two bits is 1	x ^ b
+// <<	Zero fill left shift	Shift left by pushing zeros in from the right	x << 2
+// >>	Signed right shift	Shift right by pushing copies of the leftmost bit in from the left, and let the rightmost bits fall off	x >> 2
